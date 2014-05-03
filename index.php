f9b099d572d57cac9b03e359ac8ff2d7bebec980
Mike Kohn <kohnmd@gmail.com>
2014-05-03 16:28:04 -0600

    Updated GA for new domain.

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-4010396-5', 'rhymeswithmilk.com');
+    ga('create', 'UA-4010396-7', 'mkohn.net');
     ga('send', 'pageview');
     </script>
 </body>
