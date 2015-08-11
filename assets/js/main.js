bc4c9ed065c64626391f1d3617d7aea9f141b98d
Mike Kohn <kohnmd@gmail.com>
2015-08-10 22:58:56 -0600

    Trim whitespace :scissors:

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
     var bgChangers = document.getElementsByClassName('bgChanger');
     var preloaded = [];
     var changeToBackground = "";
-    
+
     // Watch for clicks on bgChanger links.
     for(var i=0; i<bgChangers.length; i++) {
         bgChangers[i].onclick = function() {
@@ -13,7 +13,7 @@
             return false;
         };
     }
-    
+
     // Changes background image. Calls preloadImage if necessary.
     function changeBackground(src) {
         if( preloaded.indexOf(src) === -1 ) {
@@ -23,10 +23,11 @@
             body.style.backgroundImage = 'url('+src+')';
         }
     }
-    
+
     // Displays loading graphic until an image is fully loaded.
     function preloadImage(src) {
         addLoadingOverlay();
+        setTimeout(function(){}, 1000);
         var bgImage = new Image();
         bgImage.onload = function() {
             preloaded.push(src);
@@ -40,30 +41,30 @@
         }
         bgImage.src = src;
     }
-    
+
     // Generates loading overlay and adds it to the DOM.
     function addLoadingOverlay() {
         if(document.getElementById('overlay') === null) {
             var overlay = document.createElement('div');
             overlay.id = 'overlay';
-            overlay.style.height = Math.max( body.scrollHeight, body.offsetHeight, 
+            overlay.style.height = Math.max( body.scrollHeight, body.offsetHeight,
                    html.clientHeight, html.scrollHeight, html.offsetHeight ) + 'px';
-            
+
             var loader = document.createElement('div');
             loader.className = 'loader';
             loader.appendChild( document.createElement('span') );
             loader.appendChild( document.createElement('span') );
             loader.appendChild( document.createElement('span') );
             overlay.appendChild(loader);
-            
+
             body.appendChild(overlay);
-            
+
             setTimeout(function() {
                 overlay.style.backgroundColor = 'rgba(0,0,0,0.8)';
             }, 100)
         }
     }
-    
+
     // Removes the loading overlay. Called once image has loaded.
     function removeLoadingOverlay() {
         if(document.getElementById('overlay') !== null) {
